84de82981b7ffbc2e13db607b240545867a21958
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-18 16:12:16 +0300

    Decided the exercise 10

---
+++ exercise_10/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Input number from 100 till 999");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number > 99) && (number < 1000))
+{
+    Console.WriteLine(number % 100 / 10);
+}
+
+
