fc0075918841dc2a78e092dff27d263698792442
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-18 16:50:10 +0300

    Decided 15 exercise

--- exercise_15/Program.cs
+++ exercise_15/Program.cs
@@ -4,4 +4,17 @@
 1 -> нет
 */
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Input day");
+int day = Convert.ToInt32(Console.ReadLine());
+if((day > 0) && (day < 7))
+{
+    Console.WriteLine("weekday");
+}
+if((day >5) && (day < 8))
+{
+    Console.WriteLine("weekend");
+}
+if((day < 1) || (day > 7))
+{
+    Console.WriteLine("error");
+}
